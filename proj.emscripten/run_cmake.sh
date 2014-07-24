5f1106bd7237b0c60e8e16ade8c4ec15a936ed63
Jonas Matser <jonas@jonasmatser.nl>
2014-07-24 23:13:34 +0200

    run_cmake.sh

--- proj.emscripten/run_cmake.sh
+++ proj.emscripten/run_cmake.sh
@@ -1,4 +1,4 @@
-cmake -DCMAKE_TOOLCHAIN_FILE=~/Development/emsdk-portable/emscripten/1.21.0/cmake/Platform/Emscripten.cmake \
+cmake -DCMAKE_TOOLCHAIN_FILE=$EMSCRIPTEN/cmake/Platform/Emscripten.cmake \
 -DEMSCRIPTEN=ON \
 -G "Unix Makefiles" \
 ..
